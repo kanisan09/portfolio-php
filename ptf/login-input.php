a88910fa5e3a2058b97545fe8116f0352b866c56
kanisan09 <194599381+kanisan09@users.noreply.github.com>
2025-03-05 16:59:07 +0900

    更新

---
+++ ptf/login-input.php
@@ -0,0 +1,29 @@
+<?php require 'header.php'; ?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+    <link rel="stylesheet" href="log.css">
+</head>
+<body>
+    <div class="login-group">
+        <h1>ログイン</h1>
+        <div class="login">
+        <form action="login-output.php" method='post'>
+            <label for="username">ユーザー名</label>
+            <input type="text" name="login">
+            </div><br>
+            <div class="login">
+                <label for="password">パスワード</label>
+                <input type="text" name="password"><br>
+            </div>
+            <input type="submit" value="ログイン">
+        </form>
+        </div>
+    </div>
+</body>
+</html>
