14c87cfe9b97bf3f60a9d0a8e97c967d7ae7d76c
kanisan09 <194599381+kanisan09@users.noreply.github.com.>
2025-03-09 14:16:05 +0900

    変更

--- ptf/cart-delete.php
+++ ptf/cart-delete.php
@@ -3,7 +3,7 @@ session_start();
 require 'header.php';
 
 unset($_SESSION['product'][$_REQUEST['id']]);
-echo 'カートから商品を削除しました。<br>';
+echo 'カートから商品を削除しまました。<br>';
 ?>
 
 <p><a href="product.php">商品一覧へ</a></p>
